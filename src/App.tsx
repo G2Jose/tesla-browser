ceedf6f2f185c5be2fc7e36373fe04c01c6cb322
George Jose <george.jose.a@gmail.com>
2022-08-18 17:37:05 +0530

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,327 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  ImgHTMLAttributes,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
+import logo from './logo.svg'
+import prime from './prime.png'
+import appleTv from './appleTv.png'
+import disneyPlus from './disneyPlus.png'
+import hboMax from './hboMax.png'
+import hulu from './hulu.jpeg'
+import netflix from './netflix.png'
+import youtube from './youtube.webp'
+import tikTok from './tikTok.svg'
+import google from './google.png'
+import './App.css'
+import useLongPress from './useLongPress'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type TSite = {
+  name: string
+  url: string
+  image: ImgHTMLAttributes<HTMLImageElement>['src']
+  style?: React.CSSProperties
 }
 
-export default App;
+const TILE_SIZE = 400
+
+function Site({
+  name,
+  url,
+  image,
+  style,
+  onRemove,
+}: TSite & { onRemove: () => void }) {
+  const { handlers } = useLongPress(onRemove, 500)
+
+  return (
+    <a
+      href={`https://www.youtube.com/redirect?q=${url}`}
+      style={{ textAlign: 'center' }}
+      {...handlers}
+    >
+      <div style={style}>
+        <img
+          src={image}
+          alt={name}
+          style={{
+            width: TILE_SIZE,
+            height: TILE_SIZE,
+            objectFit: 'cover',
+          }}
+        />
+        <p>{name}</p>
+      </div>
+    </a>
+  )
+}
+
+function AddItem({
+  onPressAdd,
+  name,
+  onNameChange,
+  onPressCancel,
+  onUrlChange,
+  url,
+}: {
+  onPressAdd: () => void
+  onPressCancel: () => void
+  name?: string
+  url?: string
+  onNameChange: React.ChangeEventHandler<HTMLInputElement>
+  onUrlChange: React.ChangeEventHandler<HTMLInputElement>
+}) {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        opacity: 0.98,
+        flexDirection: 'column',
+      }}
+    >
+      <div style={{ width: '80%' }}>
+        <div>
+          <div>
+            <label htmlFor="name" style={{ fontWeight: 'bold' }}>
+              Name
+            </label>
+          </div>
+          <div>
+            <input
+              onChange={onNameChange}
+              value={name || ''}
+              type="text"
+              id="name"
+              name="name"
+              placeholder=""
+              style={{
+                padding: '12px 12px',
+                margin: '8px 0',
+                display: 'inline-block',
+                border: '1px solid #ccc',
+                borderRadius: '4px',
+                boxSizing: 'border-box',
+                width: '100%',
+              }}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="url" style={{ fontWeight: 'bold' }}>
+              URL
+            </label>
+          </div>
+          <div>
+            <input
+              onChange={onUrlChange}
+              value={url || ''}
+              type="text"
+              id="url"
+              name="url"
+              style={{
+                padding: '12px 12px',
+                margin: '8px 0',
+                display: 'inline-block',
+                border: '1px solid #ccc',
+                borderRadius: '4px',
+                boxSizing: 'border-box',
+                width: '100%',
+              }}
+            />
+          </div>
+        </div>
+        <button
+          style={{
+            padding: '12px 12px',
+            margin: '8px 0',
+            border: 'none',
+            borderRadius: '8px',
+            boxSizing: 'border-box',
+            width: '100%',
+            cursor: 'pointer',
+          }}
+          onClick={onPressAdd}
+        >
+          Add
+        </button>
+        <button
+          style={{
+            padding: '12px 12px',
+            margin: '8px 0',
+            border: 'none',
+            borderRadius: '8px',
+            boxSizing: 'border-box',
+            width: '100%',
+            cursor: 'pointer',
+          }}
+          onClick={onPressCancel}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function addSite({ name, url, image }: TSite) {
+  const currentSitesString = localStorage.getItem('sites')
+  const currentItems = currentSitesString ? JSON.parse(currentSitesString) : []
+  const updatedItems = [...currentItems, { name, url, image }]
+  localStorage.setItem('sites', JSON.stringify(updatedItems))
+}
+
+function removeSite({ name, url }: Omit<TSite, 'image'>) {
+  const currentSitesString = localStorage.getItem('sites')
+  const currentItems: TSite[] = currentSitesString
+    ? JSON.parse(currentSitesString)
+    : []
+  const updatedItems = currentItems.filter(
+    item => item.name !== name && item.url !== url
+  )
+  localStorage.setItem('sites', JSON.stringify(updatedItems))
+}
+
+function getStoredSites() {
+  const currentSitesString = localStorage.getItem('sites')
+  return currentSitesString ? JSON.parse(currentSitesString) : []
+}
+
+const SITE_CONFIG: TSite[] = [
+  { name: 'Netflix', image: netflix, url: 'https://netflix.com' },
+  { name: 'YouTube', image: youtube, url: 'https://youtube.com' },
+  { name: 'Disney+', image: disneyPlus, url: 'https://disneyplus.com' },
+  { name: 'Apple TV+', image: appleTv, url: 'https://tv.apple.com/' },
+  { name: 'HBO Max', image: hboMax, url: 'https://www.hbomax.com/' },
+  { name: 'Amazon Prime', image: prime, url: 'https://primevideo.com' },
+  { name: 'Hulu', image: hulu, url: 'https://hulu.com' },
+  { name: 'TikTok', image: tikTok, url: 'https://tiktok.com' },
+  {
+    name: 'Google',
+    image: google,
+    url: 'https://google.com',
+  },
+].sort((a, b) => (a.name < b.name ? -1 : 1))
+
+function App() {
+  const [sites, setSites] = useState<TSite[]>([])
+
+  const refreshSites = useCallback(() => {
+    setSites([...getStoredSites()])
+  }, [])
+
+  useEffect(() => {
+    const storedSites = getStoredSites()
+    if (!storedSites.length) {
+      SITE_CONFIG.map(x => addSite(x))
+      refreshSites()
+    }
+  }, [])
+
+  useEffect(refreshSites, [refreshSites])
+
+  const [isAdding, setIsAdding] = useState(false)
+
+  const [name, setName] = useState<string | undefined>(undefined)
+  const [url, setUrl] = useState<string | undefined>(undefined)
+
+  const startAdding = useCallback(() => setIsAdding(true), [])
+
+  const onPressCancel = useCallback(() => {
+    setIsAdding(false)
+  }, [])
+
+  const onPressAdd = useCallback(
+    (name?: string, url?: string) => {
+      setIsAdding(false)
+
+      console.log({ url, name })
+
+      if (name && url) {
+        const imageUrl = `https://logo.clearbit.com/${url}?size=800`
+        addSite({ name, url, image: imageUrl })
+        setName(undefined)
+        setUrl(undefined)
+        refreshSites()
+      }
+    },
+    [refreshSites]
+  )
+
+  const onRemove = useCallback(
+    (site: TSite) => {
+      // eslint-disable-next-line no-restricted-globals
+      const value = confirm(`Would you like to remove ${site.name}?`)
+      if (value) {
+        removeSite({ name: site.name, url: site.url })
+        refreshSites()
+      }
+    },
+    [refreshSites]
+  )
+
+  return (
+    <div className="App" style={{ margin: '20px' }}>
+      {isAdding && (
+        <AddItem
+          onPressAdd={() => onPressAdd(name, url)}
+          onNameChange={e => setName(e.target.value)}
+          onUrlChange={e => setUrl(e.target.value)}
+          url={url}
+          name={name}
+          onPressCancel={() => onPressCancel()}
+        />
+      )}
+      <div
+        style={{
+          flexDirection: 'row',
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+        }}
+      >
+        <>
+          {sites.map((x, i) => (
+            <Site
+              {...x}
+              key={i}
+              style={{ marginBottom: 50, margin: 4 }}
+              onRemove={() => onRemove(x)}
+            />
+          ))}
+
+          <button
+            style={{
+              margin: 4,
+              width: TILE_SIZE,
+              height: TILE_SIZE,
+              borderWidth: 0,
+              fontSize: 40,
+              cursor: 'pointer',
+            }}
+            onClick={startAdding}
+          >
+            +
+          </button>
+          {new Array(4).fill(undefined).map((x, i) => (
+            <div
+              key={i}
+              style={{ width: TILE_SIZE, height: 0, backgroundColor: 'red' }}
+            ></div>
+          ))}
+        </>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/useLongPress.ts
@@ -0,0 +1,36 @@
+import { SyntheticEvent, useRef, useState } from 'react'
+
+export default function useLongPress(
+  onLongPress: () => void,
+  timeout: number = 500
+) {
+  const startTime = useRef<number | null>(null)
+  const onMouseDown = () => {
+    const now = Date.now()
+    startTime.current = now
+
+    setTimeout(() => {
+      if (startTime.current && Date.now() - startTime.current >= timeout) {
+        onLongPress()
+      } else {
+        startTime.current = null
+      }
+    }, timeout)
+  }
+
+  const onMouseUp = () => {
+    const now = Date.now()
+    if (startTime.current && now - startTime.current >= timeout) {
+      onLongPress()
+    }
+
+    startTime.current = null
+  }
+
+  return {
+    handlers: {
+      onMouseUp,
+      onMouseDown,
+    },
+  }
+}
